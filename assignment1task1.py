8ac2cbf061a90b94636817e8f047b11594757339
krishnawhoopsie <krishspoptani@gmail.com>
2026-02-27 13:32:50 +0530

    Add files via upload

---
+++ assignment1task1.py
@@ -0,0 +1,14 @@
+print("\n")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a + b
+sub = a - b
+multi = a * b
+divi = a / b
+
+print("\n")
+print(f"Addition: {add}")
+print(f"Addition: {sub}")
+print(f"Addition: {multi}")
+print(f"Addition: {divi}")
---
+++ assignment1task2.py
@@ -0,0 +1,5 @@
+fname = input("Enter first your name: ")
+lname = input("Enter last your name: ")
+full_name = fname + " " + lname
+
+print(f"Hello, {full_name}! Welcome to python program.")
